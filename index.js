b6435736560183976292598d99ea6c6ce218e0c1
khumayunkh <hasanovhumayun15@gmail.com>
2022-06-03 07:53:04 +0500

    Map

--- index.js
+++ index.js
@@ -1,44 +1,69 @@
-console.log('hello world')
+// console.log('hello world')
 
-let a = new Set([4]) 
-console.log(a)
-a.add(3)
-console.log(a)
-a.add(3) // false
-console.log(a) 
-a.delete(4)
-console.log(a) //[3]
-a.add(4)
-a.add(1)
-a.add(8)
-a.clear()
-console.log(a) // []
-a.add('hello').add(2).add(3)
-console.log(a) // ['hello', 2, 3]
-console.log(a.has(3)) // true
-console.log(a.has('2')) // false
+// let a = new Set([4]) 
+// console.log(a)
+// a.add(3)
+// console.log(a)
+// a.add(3) // false
+// console.log(a) 
+// a.delete(4)
+// console.log(a) //[3]
+// a.add(4)
+// a.add(1)
+// a.add(8)
+// a.clear()
+// console.log(a) // []
+// a.add('hello').add(2).add(3)
+// console.log(a) // ['hello', 2, 3]
+// console.log(a.has(3)) // true
+// console.log(a.has('2')) // false
 
-let x = new Set([1,2,3,4,5])
+// let x = new Set([1,2,3,4,5])
+// console.log(x)
+// let sum = 0
+
+// for(let p of x){
+//     sum += p
+// }
+
+// console.log(sum)
+
+// // let y = [...x]
+// // console.log(typeof y) // object 
+// // console.log(typeof x) // object
+
+
+// let t = 1
+// x.forEach(n=> t *=n)
+// console.log(t)
+
+// let y  = [1, 2, 3, 4, 5]
+// let o = 1
+// y.forEach(n=>o*=n)
+// console.log(o) // 120
+
+//=====================
+//Map
+
+let x = new Map()
+x.set(3,23)
+x.set('one', 3)
 console.log(x)
-let sum = 0
+console.log(x.get(3))
+x.set('two', 24)
+console.log(x.get('two'))
+let y = [...x.values()]
+console.log(y) // [23, 3, 24]
+let z = [...x.keys()]
+console.log(z) // [3, 'one', 'two']
+console.log(z[1]) // one
 
-for(let p of x){
-    sum += p
+
+let o =1
+for(let p of x.values()){
+    o += p
 }
-
-console.log(sum)
-
-// let y = [...x]
-// console.log(typeof y) // object 
-// console.log(typeof x) // object
+console.log(o)  // 51
 
 
-let t = 1
-x.forEach(n=> t *=n)
-console.log(t)
-
-let y  = [1, 2, 3, 4, 5]
-let o = 1
-y.forEach(n=>o*=n)
-console.log(o) // 120
 
