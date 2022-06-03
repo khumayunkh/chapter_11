d5e8641db13236b2e31ac2c422c57bc73d3e1f75
khumayunkh <hasanovhumayun15@gmail.com>
2022-06-03 09:30:37 +0500

    JSON

--- index.js
+++ index.js
@@ -45,25 +45,51 @@
 //=====================
 //Map
 
-let x = new Map()
-x.set(3,23)
-x.set('one', 3)
+// let x = new Map()
+// x.set(3,23)
+// x.set('one', 3)
+// console.log(x)
+// console.log(x.get(3))
+// x.set('two', 24)
+// console.log(x.get('two'))
+// let y = [...x.values()]
+// console.log(y) // [23, 3, 24]
+// let z = [...x.keys()]
+// console.log(z) // [3, 'one', 'two']
+// console.log(z[1]) // one
+
+
+// let o =1
+// for(let p of x.values()){
+//     o += p
+// }
+// console.log(o)  // 51
+
+
+
+// let x = new Int8Array([12,4,3,120])
+// console.log(x) // [12,4,3,120]
+
+// let y = new BigInt64Array(342,23,132)
+// console.log(y)
+
+let z = new Date(2010, 0, 1,23,3,2)
+console.log(z) // 01.01.2010 23:03:02
+
+let x = 'hello'
 console.log(x)
-console.log(x.get(3))
-x.set('two', 24)
-console.log(x.get('two'))
-let y = [...x.values()]
-console.log(y) // [23, 3, 24]
-let z = [...x.keys()]
-console.log(z) // [3, 'one', 'two']
-console.log(z[1]) // one
+let d =new Date()
+console.log(d)
+console.log(d.getMonth(d.getMonth() + 2))
 
 
-let o =1
-for(let p of x.values()){
-    o += p
-}
-console.log(o)  // 51
+let o = {a:2, b:7, f:[2,3,4], l:'jhello'}
+let s =JSON.stringify(o)
+
+console.log(s)
+console.log(JSON.parse(s))
+
+
 
 
 
