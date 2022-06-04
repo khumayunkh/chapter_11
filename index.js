125cc194052ebedaa0a5909984275e1da84ef2fa
khumayunkh <hasanovhumayun15@gmail.com>
2022-06-04 10:06:14 +0500

    get/set

--- index.js
+++ index.js
@@ -73,43 +73,65 @@
 // let y = new BigInt64Array(342,23,132)
 // console.log(y)
 
-let z = new Date(2010, 0, 1,23,3,2)
-console.log(z) // 01.01.2010 23:03:02
+// let z = new Date(2010, 0, 1,23,3,2)
+// console.log(z) // 01.01.2010 23:03:02
 
-let x = 'hello'
-console.log(x)
+// let x = 'hello'
+// console.log(x)
+// let d =new Date()
+// console.log(d)
+// console.log(d.getMonth(d.getMonth() + 2))
+
+
+// let o = {a:2, b:7, f:[2,3,4], l:'jhello'}
+// let s =JSON.stringify(o)
+
+// console.log(s)
+// console.log(JSON.parse(s))
+
+
+// let euro = Intl.NumberFormat('es',{style:'currency', currency:'EUR'})
+// console.log(euro.format(10)) // 10,00 €
+// let pounds = Intl.NumberFormat('en',{style:'currency',currency:'GBP'})
+// console.log(pounds.format(20)) // £20.00
+
+
+// let data = [0.09, 9, 10, 0.8]
+// let formatData = Intl.NumberFormat(undefined,{style:'percent', minimumFractionDigits:3,maximumSignificantDigits:2}).format
+// console.log(data.map(formatData)) // [ "9%", "900%", "1,000%", "80%" ]
+
+// let arabic =Intl.NumberFormat('ar',{useGrouping:false}).format
+// console.log(arabic(123452)) // ١٢٣٤٥٢
+
+// let hindi = Intl.NumberFormat('hi-IN-u-nu-deva').format
+// console.log(hindi(1234)) // १,२३४
+
+// let b =new Date()
+// let opt = {weekday: 'long', month:'numeric', year:'numeric', day:'numeric'}
+
+// console.log(Intl.DateTimeFormat('en-US', opt).format(d)) //Friday, 6/3/2022
+// console.log(Intl.DateTimeFormat('en-u-ca-islamic',opt ).format(d)) // Friday, 11/4/1443 AH
+
 let d =new Date()
-console.log(d)
-console.log(d.getMonth(d.getMonth() + 2))
+let p ={weekday:'long', month : 'numeric', year:'numeric', day: 'numeric'}
+console.log(Intl.DateTimeFormat('en-Us', p).format(d))
 
+let euro = Intl.NumberFormat('en', {style:'currency',currency:'EUR'}).format(10)
+console.log(euro) // €10.00
 
-let o = {a:2, b:7, f:[2,3,4], l:'jhello'}
-let s =JSON.stringify(o)
+let pr = [21,23,3,2]
+let forma = Intl.NumberFormat(undefined,{style:'percent', minimumFractionDigits:2, maximumFractionDigits:3}).format
+console.log(pr.map(forma)) // [ "2,100.00%", "2,300.00%", "300.00%", "200.00%" ]
 
-console.log(s)
-console.log(JSON.parse(s))
+let b = Intl.NumberFormat('hi-IN-u-nu-deva').format
+console.log(b(1234))
 
+let x =new Set()
+x.add(3).add(4).add(3)
+console.log(x) // [3,4]
 
-let euro = Intl.NumberFormat('es',{style:'currency', currency:'EUR'})
-console.log(euro.format(10)) // 10,00 €
-let pounds = Intl.NumberFormat('en',{style:'currency',currency:'GBP'})
-console.log(pounds.format(20)) // £20.00
-
-
-let data = [0.09, 9, 10, 0.8]
-let formatData = Intl.NumberFormat(undefined,{style:'percent', minimumFractionDigits:3,maximumSignificantDigits:2}).format
-console.log(data.map(formatData)) // [ "9%", "900%", "1,000%", "80%" ]
-
-let arabic =Intl.NumberFormat('ar',{useGrouping:false}).format
-console.log(arabic(123452)) // ١٢٣٤٥٢
-
-let hindi = Intl.NumberFormat('hi-IN-u-nu-deva').format
-console.log(hindi(1234)) // १,२३४
-
-let b =new Date()
-let opt = {weekday: 'long', month:'numeric', year:'numeric', day:'numeric'}
-
-Intl.DateTimeFormat('en-US', opt).format(d)
-console.log(Intl.DateTimeFormat('en-US', opt).format(d)) //Friday, 6/3/2022
-console.log(Intl.DateTimeFormat('en-u-ca-islamic',opt ).format(d)) // Friday, 11/4/1443 AH
-
+let y = new Map()
+y.set(2,2)
+console.log(y) // (2 -> 2)
+y.set('one', 1)
+console.log(y.get('one')) // 1
